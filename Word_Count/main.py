f09a43faab8e381c1d36afbc4ec862a83324527b
DJ_Juraski <daironrjuraski@gmail.com>
2020-09-18 20:32:35 -0300

    Add files via upload

--- Word_Count/main.py
+++ Word_Count/main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # código reutilizado de uma implementação do google exercicies
 
 ##################################################################
@@ -13,83 +14,81 @@
 
 import string
 import sys
-import time
 from multiprocessing import Pool
-from threading import Lock
-
 import nltk
 
-mutex = Lock()
 nltk.download('stopwords')
 stopwords = nltk.corpus.stopwords.words('portuguese')
 
-filenames = ['test2.txt', 'test.txt']
+filenames = ['test.txt', 'test2.txt']
 
 
-#contagem das palavras e tratamento
-def word_counted_dict(filename):
+def read_file(filename: str) -> str:
+    with open(filename, 'r', encoding='utf-8') as fd:
+        return fd.readlines()
+
+
+# contagem das palavras e tratamento
+def word_counted_dict(filename: str) -> dict:
     word_counted = {}
-    input_file = open(filename, 'r')
+    input_file = read_file(filename)
+
     for line in input_file:
-        words = line.split()
-        for word in words:
+        for word in line.split():
             if word not in stopwords:
-                word = word.lower()
-                word = word.strip(string.punctuation)
+                word = word.lower().strip(string.punctuation)
+
                 if word not in word_counted:
                     word_counted[word] = 1
                 else:
                     word_counted[word] = word_counted[word] + 1
 
-    input_file.close()
     return word_counted
 
 
-#imprime alfabeticamente ordenado
-def print_words(filename):
+# imprime alfabeticamente ordenado
+def print_words(filename: str):
     word_counted = word_counted_dict(filename)
 
-    words = sorted(word_counted.keys())
-
-    for word in words:
-        mutex.acquire()
+    for word in sorted(word_counted.keys()):
         print(word, word_counted[word])
-        mutex.release()
 
 
-#Imprime ordenado por quantidade
-def print_top(filename):
+# Imprime ordenado por quantidade
+def print_top(filename: str):
     word_counted = word_counted_dict(filename)
 
-    items = sorted(word_counted.items(), key=get_count, reverse=True)
+    # Max of 20
+    for item in sorted(word_counted.items(), key=get_count, reverse=True)[:20]:
+        print('Top', item, item[0], item[1])
 
-    for item in items[:20]:  # Max of 20
-        print(item[0], item[1])
 
-
-def get_count(word_counted_tuple):
+def get_count(word_counted_tuple: tuple):
     return word_counted_tuple[1]
 
 
 def main():
-    start = time.time()
     if len(sys.argv) != 3:
-        print('usage: ./main.py {--count | --topcount} file')
+        print(
+            f'usage: {sys.argv[0]} < --count | --topcount> <file>', file=sys.stderr)
         sys.exit(1)
 
     option = sys.argv[1]
     filename = sys.argv[2]
 
-    if option == '--count':
-        print_words(filename)
-    elif option == '--topcount':
-        print_top(filename)
-    else:
-        print('unknown option: ' + option)
-        sys.exit(1)
+    options = {
+        '--count': print_words,
+        '--topcount': print_top
+    }
 
-    end = time.time()
-    print('\nTempo de execução: ', end-start)
+    execFunction = options.get(option, None)
+
+    if execFunction:
+        execFunction(filename)
+        return
+
+    print('unknown option:', option)
+    sys.exit(1)
 
 
 def pool_func():
@@ -98,4 +97,5 @@ def pool_func():
 
 
 if __name__ == '__main__':
+    print('test')
     pool_func()
